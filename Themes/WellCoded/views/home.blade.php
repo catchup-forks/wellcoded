f1441b123842df280ffb045e9988d046bbe755f3
Nicolas Widart <n.widart@gmail.com>
2016-01-02 14:53:44 +0100

    Showing podcasts on homepage

--- Themes/WellCoded/views/home.blade.php
+++ Themes/WellCoded/views/home.blade.php
@@ -1,16 +1,47 @@
 @extends('layouts.master')
 
 @section('title')
-    {{ $page->title }} | @parent
+    Les podcasts | @parent
 @stop
+
 @section('meta')
-    <meta name="title" content="{{ $page->meta_title}}" />
-    <meta name="description" content="{{ $page->meta_description }}" />
+    {{-- @todo --}}
 @stop
 
 @section('content')
-    <div class="row">
-        <h1>{{ $page->title }}</h1>
-        {!! $page->body !!}
-    </div>
+    <div id="blue">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                    <h3>Podcasts</h3>
+                </div>
+            </div><!-- /row -->
+        </div> <!-- /container -->
+    </div><!-- /blue -->
+
+    <div class="container mtb">
+        <div class="row">
+            <div class="col-lg-8 col-lg-offset-2">
+                @foreach($podcasts as $podcast)
+                    <h3 class="ctitle">
+                        <a href="{{ route('podcasts.show', [$podcast->id, $podcast->present()->slug]) }}">
+                            {{ $podcast->title }}
+                        </a>
+                    </h3>
+
+                    <p><small>Posted: {{ $podcast->present()->published_at }}.</small></p>
+                    @include('podcast::frontend._.player')
+                    <p>
+                        {!! $podcast->present()->excerpt !!}
+                    </p>
+                    <p class="text-right">
+                        <a href="{{ route('podcasts.show', [$podcast->id, $podcast->present()->slug]) }}">
+                            Voir plus <i class="fa fa-chevron-right"></i>
+                        </a>
+                    </p>
+                @endforeach
+            </div><! --/col-lg-8 -->
+
+        </div><! --/row -->
+    </div><! --/container -->
 @stop
