fdd5bc89365ace54f9c938967d1ec96e6a19397b
Nicolas Widart <n.widart@gmail.com>
2016-01-02 13:41:25 +0100

    Extract player partial

---
+++ Modules/Podcast/Resources/views/frontend/_/player.blade.php
@@ -0,0 +1,3 @@
+<div class="player">
+    <audio class="mejs-player" src="{{ $podcast->files->first()->path }}" data-mejsoptions='{"features": ["playpause","progress","current","duration","volume", "speed"]}'></audio>
+</div>
