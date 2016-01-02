b54ec53999aa20c4320bd9a3c024f3f854fc3334
Nicolas Widart <n.widart@gmail.com>
2016-01-02 15:12:10 +0100

    Extract podcast index to partial

---
+++ Modules/Podcast/Resources/views/frontend/_/podcast-index.blade.php
@@ -0,0 +1,25 @@
+<div class="container mtb">
+    <div class="row">
+        <div class="col-lg-6 col-lg-offset-3" style="padding-top: 100px;">
+            @foreach($podcasts as $podcast)
+            <h3 class="ctitle">
+                <a href="{{ route('podcasts.show', [$podcast->id, $podcast->present()->slug]) }}">
+                    {{ $podcast->title }}
+                </a>
+            </h3>
+
+            <p><small>Posted: {{ $podcast->present()->published_at }}.</small></p>
+            @include('podcast::frontend._.player')
+            <p>
+                {!! $podcast->present()->excerpt !!}
+            </p>
+            <p class="text-right">
+                <a href="{{ route('podcasts.show', [$podcast->id, $podcast->present()->slug]) }}">
+                    Voir plus <i class="fa fa-chevron-right"></i>
+                </a>
+            </p>
+            @endforeach
+        </div><! --/col-lg-8 -->
+
+    </div><! --/row -->
+</div><! --/container -->
