3857187d13bd838af07f510686893f4b997be50a
Julien Tant <julien@craftyx.fr>
2016-01-13 02:21:33 +0100

    make title dynamic into rss feed

--- Modules/Podcast/Resources/views/frontend/rss.blade.php
+++ Modules/Podcast/Resources/views/frontend/rss.blade.php
@@ -2,7 +2,7 @@
 <rss version="2.0" xmlns:atom="http://www.w3.org/2005/Atom/" xmlns:content="http://purl.org/rss/1.0/modules/content/" xmlns:itunes="http://www.itunes.com/dtds/podcast-1.0.dtd">
     <channel>
         <atom:link rel="self" type="application/atom+xml" href="{{ URL::current() }}" title="MP3 Audio"/>
-        <title>The Wellcoded Podcast</title>
+        <title>{{ setting('core::site-name') }}</title>
         <generator>https://wellcoded.io</generator>
         <description>
             Le Podcast Wellcoded traite des sujets tels que le freelancing et le développement web. Les deux hosts sont Julien Tant et Nicolas Widart, deux freelances Laravel basés en Europe.
