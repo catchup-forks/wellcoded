9e198dc1b0c20487373adf8bc0fafe1fe6202cad
Julien Tant <julien@craftyx.fr>
2016-01-14 11:53:12 +0100

    try using cdata

--- Modules/Podcast/Resources/views/frontend/rss.blade.php
+++ Modules/Podcast/Resources/views/frontend/rss.blade.php
@@ -30,27 +30,19 @@
         <itunes:category text="Technology"/>
         @foreach($podcasts as $podcast)
             <item>
-                <title>{!! $podcast->title  !!}</title>
+                <title><![CDATA[{!! $podcast->title  !!}]]></title>
                 <guid isPermaLink="false">{{ $podcast->id }}</guid>
                 <link>{{ $podcast->present()->url }}</link>
-                <description>
-                    {!! html_entity_decode(htmlspecialchars_decode($podcast->present()->excerpt, ENT_QUOTES)) !!}
-                </description>
-                <content:encoded>
-                    <![CDATA[
-                    {!! $podcast->description !!}
-                    ]]>
-                </content:encoded>
+                <description><![CDATA[{!! $podcast->present()->excerpt !!}]]></description>
+                <content:encoded><![CDATA[{!! $podcast->description !!}]]></content:encoded>
                 <pubDate>{{ $podcast->published_at->format(DateTime::RSS) }}</pubDate>
                 <author>Julien Tant, Nicolas Widart</author>
                 <enclosure url="{{ $podcast->present()->mp3url }}" length="{{ $podcast->files()->first()->filesize }}" type="audio/mpeg"/>
                 <itunes:author>Julien Tant, Nicolas Widart</itunes:author>
                 <itunes:image href="{{ asset('assets/media/wellcoded.png') }}"/>
                 <itunes:duration>{{ $podcast->duration }}</itunes:duration>
-                <itunes:summary>{!! html_entity_decode(htmlspecialchars_decode($podcast->present()->excerpt, ENT_QUOTES)) !!}
-                </itunes:summary>
-                <itunes:subtitle>{!! html_entity_decode(htmlspecialchars_decode($podcast->present()->excerpt, ENT_QUOTES)) !!}
-                </itunes:subtitle>
+                <itunes:summary><![CDATA[{!! $podcast->present()->excerpt !!}]]></itunes:summary>
+                <itunes:subtitle><![CDATA[{!! $podcast->present()->excerpt !!}]]></itunes:subtitle>
                 <itunes:keywords>{{ $podcast->tags }}</itunes:keywords>
                 <itunes:explicit>no</itunes:explicit>
             </item>
